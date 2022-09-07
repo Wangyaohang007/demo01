0e58b150eded82b0a3af4e6f49db88155b58f4fe
Wangyaohang007 <2963415455@qq.com>
2022-09-07 23:17:03 +0800

    find images

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ gulidMenuList.forEach((menu,i)=>{
 	gulidStr += `<a href="#" key=${i} class="${i===0?'select':''}" onClick='toggleMeun(${i})'>${menu.name}</a>`
 })
 $('#GulidTitle').html(gulidStr)
-// $('#GulidTitle').find('a').on('click',toggleMeun)
+$('#GulidTitle').find('a').on('click',toggleMeun)
 function toggleMeun(e,i){
 	console.log(e)
 	var target = e.target
@@ -19,26 +19,26 @@ function toggleMeun(e,i){
 }
 
 var products=[
-	{img:'07.jpg',title:'番茄牛腩面'},
-	{img:'08.jpg',title:'蒜泥白肉'},
-	{img:'09.jpg',title:'酱香带鱼'},
-	{img:'10.jpg',title:'家常烧鱿鱼'},
-	{img:'11.jpg',title:'饺子皮锅巴'},
-	{img:'12.jpg',title:'凉拌黄瓜皮蛋'},
-	{img:'13.jpg',title:'黄豆烧五花肉'},
-	{img:'14.jpg',title:'爆炒脆鱼肠'},
-	{img:'15.jpg',title:'鼓汁猪颈肉'},
-	{img:'16.jpg',title:'烧烤味杏鲍菇'},
-	{img:'17.jpg',title:'薯塔薯条'},
-	{img:'18.jpg',title:'凉拌猪肝'},
-	{img:'19.jpg',title:'三姆牛肉卷'},
-	{img:'20.jpg',title:'鱼片羹'},
-	{img:'21.jpg',title:'鸡肉小炒'},
-	{img:'22.jpg',title:'豆角焖面'},
+	{img:'1_1.jpg',title:'番茄牛腩面'},
+	{img:'1_2.jpg',title:'蒜泥白肉'},
+	{img:'1_3.jpg',title:'酱香带鱼'},
+	{img:'1_4.jpg',title:'家常烧鱿鱼'},
+	{img:'1_1.jpg',title:'饺子皮锅巴'},
+	{img:'1_2.jpg',title:'凉拌黄瓜皮蛋'},
+	{img:'1_3.jpg',title:'黄豆烧五花肉'},
+	{img:'1_4.jpg',title:'爆炒脆鱼肠'},
+	{img:'1_1.jpg',title:'鼓汁猪颈肉'},
+	{img:'1_2.jpg',title:'烧烤味杏鲍菇'},
+	{img:'1_3.jpg',title:'薯塔薯条'},
+	{img:'1_4.jpg',title:'凉拌猪肝'},
+	{img:'1_1.jpg',title:'三姆牛肉卷'},
+	{img:'1_2.jpg',title:'鱼片羹'},
+	{img:'1_3.jpg',title:'鸡肉小炒'},
+	{img:'1_4.jpg',title:'豆角焖面'},
 ]
 var str=''
 products.forEach((item,index)=>{
-	str+=`<li><a href="#"><img src="images/home/${item.img}" alt=""><p>${item.title}</p></a></li>`
+	str+=`<li><a href="#"><img src="images/main/${item.img}" alt=""><p>${item.title}</p></a></li>`
 })
 var ProductsList = document.getElementById('ProductsList')
 ProductsList.innerHTML=str
@@ -61,7 +61,7 @@ var summary=[
 ]
 var str1=''
 summary.forEach((item,index)=>{
-	str1+=`<li><a href="#"><img src="images/home/${item.img}" alt=""><p>${item.title}</p></a></li>`
+	str1+=`<li><a href="#"><img src="images/main/${item.img}" alt=""><p>${item.title}</p></a></li>`
 })
 var Summarylist = document.getElementById('Summarylist')
 Summarylist.innerHTML=str1
